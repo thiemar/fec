13aa2ed1f36c5db6a2f41bf1c019bf22dff3c5be
Daniel Dyer <danielrd@mac.com>
2017-09-25 23:40:32 +1000

    Suppress clang warnings

--- test/TestReedSolomonEncoder.cpp
+++ test/TestReedSolomonEncoder.cpp
@@ -12,7 +12,6 @@ static std::array<T, sizeof...(Is)> poly_compare_helper(std::integer_sequence<T,
 
 TEST(ReedSolomonEncoderTest, Generator) {
     using TestEncoder = Thiemar::ReedSolomon::ReedSolomonEncoder<8u, Thiemar::ReedSolomon::Polynomials::m_8_285, 8u>;
-    using TestGaloisField = Thiemar::GaloisField<8u, Thiemar::ReedSolomon::Polynomials::m_8_285>;
 
     uint8_t generator[] = { 1u, 227u, 44u, 178u, 71u, 172u, 8u, 224u, 37u };
 
