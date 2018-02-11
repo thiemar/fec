22ca179369f9b082defdfc026fe41eb71f274106
Daniel Dyer <danielrd@mac.com>
2018-02-12 01:15:02 +1100

    Optimise partial SIMD loads

--- include/FEC/PolarSIMD_x86.h
+++ include/FEC/PolarSIMD_x86.h
@@ -25,6 +25,64 @@ SOFTWARE.
 #include <x86intrin.h>
 
 template <std::size_t Nv>
+static inline __m128i load_vec(const int8_t *data) {
+    /* Ensure Nv is a power of two and greater than one. */
+    static_assert(Nv > 1u && Detail::calculate_hamming_weight(Nv) == 1u,
+        "Block size must be a power of two and greater than one");
+    /* Ensure Nv is no greater than eight. */
+    static_assert(Nv <= 16u, "Block size must be sixteen or less");
+
+    if constexpr (Nv == 16u) {
+        return _mm_setr_epi8(
+            data[0u], data[1u], data[2u], data[3u],
+            data[4u], data[5u], data[6u], data[7u],
+            data[8u], data[9u], data[10u], data[11u],
+            data[12u], data[13u], data[14u], data[15u]);
+    } else if constexpr (Nv == 8u) {
+        return _mm_setr_epi8(
+            data[0u], data[1u], data[2u], data[3u],
+            data[4u], data[5u], data[6u], data[7u],
+            0, 0, 0, 0, 0, 0, 0, 0);
+    } else if constexpr (Nv == 4u) {
+        return _mm_setr_epi8(
+            data[0u], data[1u], data[2u], data[3u],
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    } else {
+        return _mm_setr_epi8(data[0u], data[1u],
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+}
+
+template <std::size_t Nv>
+static inline __m128i load_vec(const uint8_t *data) {
+    /* Ensure Nv is a power of two and greater than one. */
+    static_assert(Nv > 1u && Detail::calculate_hamming_weight(Nv) == 1u,
+        "Block size must be a power of two and greater than one");
+    /* Ensure Nv is no greater than eight. */
+    static_assert(Nv <= 16u, "Block size must be sixteen or less");
+
+    if constexpr (Nv == 16u) {
+        return _mm_setr_epi8(
+            data[0u], data[1u], data[2u], data[3u],
+            data[4u], data[5u], data[6u], data[7u],
+            data[8u], data[9u], data[10u], data[11u],
+            data[12u], data[13u], data[14u], data[15u]);
+    } else if constexpr (Nv == 8u) {
+        return _mm_setr_epi8(
+            data[0u], data[1u], data[2u], data[3u],
+            data[4u], data[5u], data[6u], data[7u],
+            0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u);
+    } else if constexpr (Nv == 4u) {
+        return _mm_setr_epi8(
+            data[0u], data[1u], data[2u], data[3u],
+            0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u);
+    } else {
+        return _mm_setr_epi8(data[0u], data[1u],
+            0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u);
+    }
+}
+
+template <std::size_t Nv>
 struct f_op_container<int8_t, Nv> {
     static std::array<int8_t, Nv / 2u> op(const std::array<int8_t, Nv> &alpha) {
         /* Ensure Nv is a power of two and greater than one. */
@@ -41,13 +99,11 @@ struct f_op_container<int8_t, Nv> {
                     _mm_sign_epi8(alpha_vec_1, alpha_vec_2)));
             }
         } else {
-            int8_t alpha_vec_1[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin(), Nv / 2u, alpha_vec_1);
-            int8_t alpha_vec_2[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin() + Nv / 2u, Nv / 2u, alpha_vec_2);
+            __m128i alpha_vec_1 = load_vec<Nv / 2u>(alpha.begin());
+            __m128i alpha_vec_2 = load_vec<Nv / 2u>(alpha.begin() + Nv / 2u);
             __m128i c = _mm_sign_epi8(_mm_min_epi8(
-                _mm_abs_epi8(*(__m128i *)alpha_vec_1), _mm_abs_epi8(*(__m128i *)alpha_vec_2)),
-                _mm_sign_epi8(*(__m128i *)alpha_vec_1, *(__m128i *)alpha_vec_2));
+                _mm_abs_epi8(alpha_vec_1), _mm_abs_epi8(alpha_vec_2)),
+                _mm_sign_epi8(alpha_vec_1, alpha_vec_2));
             std::copy_n((int8_t *)&c, Nv / 2u, out.begin());
         }
 
@@ -70,12 +126,7 @@ struct f_op_r1_container<int8_t, Nv> {
                 _mm_storeu_si128((__m128i *)&out[i], _mm_sign_epi8(alpha_vec_1, alpha_vec_2));
             }
         } else {
-            int8_t alpha_vec_1[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin(), Nv / 2u, alpha_vec_1);
-            int8_t alpha_vec_2[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin() + Nv / 2u, Nv / 2u, alpha_vec_2);
-
-            __m128i c = _mm_sign_epi8(*(__m128i *)alpha_vec_1, *(__m128i *)alpha_vec_2);
+            __m128i c = _mm_sign_epi8(load_vec<Nv / 2u>(alpha.begin()), load_vec<Nv / 2u>(alpha.begin() + Nv / 2u));
             std::copy_n((int8_t *)&c, Nv / 2u, out.begin());
         }
 
@@ -102,17 +153,13 @@ struct g_op_container<int8_t, Nv> {
                     _mm_cmpeq_epi8(beta_vec, _mm_setzero_si128())));
             }
         } else {
-            int8_t alpha_vec_1[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin(), Nv / 2u, alpha_vec_1);
-            int8_t alpha_vec_2[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin() + Nv / 2u, Nv / 2u, alpha_vec_2);
-            int8_t beta_vec[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(beta, Nv / 2u, beta_vec);
-
+            __m128i alpha_vec_1 = load_vec<Nv / 2u>(alpha.begin());
+            __m128i alpha_vec_2 = load_vec<Nv / 2u>(alpha.begin() + Nv / 2u);
+            __m128i beta_vec = load_vec<Nv / 2u>(beta);
             __m128i c = _mm_blendv_epi8(
-                _mm_subs_epi8(*(__m128i *)alpha_vec_2, *(__m128i *)alpha_vec_1),
-                _mm_adds_epi8(*(__m128i *)alpha_vec_2, *(__m128i *)alpha_vec_1),
-                _mm_cmpeq_epi8(*(__m128i *)beta_vec, _mm_setzero_si128()));
+                _mm_subs_epi8(alpha_vec_2, alpha_vec_1),
+                _mm_adds_epi8(alpha_vec_2, alpha_vec_1),
+                _mm_cmpeq_epi8(beta_vec, _mm_setzero_si128()));
             std::copy_n((int8_t *)&c, Nv / 2u, out.begin());
         }
 
@@ -135,12 +182,7 @@ struct g_op_0_container<int8_t, Nv> {
                 _mm_storeu_si128((__m128i *)&out[i], _mm_adds_epi8(alpha_vec_2, alpha_vec_1));
             }
         } else {
-            int8_t alpha_vec_1[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin(), Nv / 2u, alpha_vec_1);
-            int8_t alpha_vec_2[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin() + Nv / 2u, Nv / 2u, alpha_vec_2);
-
-            __m128i c = _mm_adds_epi8(*(__m128i *)alpha_vec_2, *(__m128i *)alpha_vec_1);
+            __m128i c = _mm_adds_epi8(load_vec<Nv / 2u>(alpha.begin() + Nv / 2u), load_vec<Nv / 2u>(alpha.begin()));
             std::copy_n((int8_t *)&c, Nv / 2u, out.begin());
         }
 
@@ -163,12 +205,7 @@ struct g_op_1_container<int8_t, Nv> {
                 _mm_storeu_si128((__m128i *)&out[i], _mm_subs_epi8(alpha_vec_2, alpha_vec_1));
             }
         } else {
-            int8_t alpha_vec_1[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin(), Nv / 2u, alpha_vec_1);
-            int8_t alpha_vec_2[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin() + Nv / 2u, Nv / 2u, alpha_vec_2);
-
-            __m128i c = _mm_subs_epi8(*(__m128i *)alpha_vec_2, *(__m128i *)alpha_vec_1);
+            __m128i c = _mm_subs_epi8(load_vec<Nv / 2u>(alpha.begin() + Nv / 2u), load_vec<Nv / 2u>(alpha.begin()));
             std::copy_n((int8_t *)&c, Nv / 2u, out.begin());
         }
 
@@ -190,12 +227,7 @@ struct h_op_container<int8_t, Nv> {
                 _mm_storeu_si128((__m128i *)&beta[i], _mm_xor_si128(beta_vec_1, beta_vec_2));
             }
         } else {
-            int8_t beta_vec_1[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(beta, Nv / 2u, beta_vec_1);
-            int8_t beta_vec_2[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(beta + Nv / 2u, Nv / 2u, beta_vec_2);
-
-            __m128i c = _mm_xor_si128(*(__m128i *)beta_vec_1, *(__m128i *)beta_vec_2);
+            __m128i c = _mm_xor_si128(load_vec<Nv / 2u>(beta), load_vec<Nv / 2u>(beta + Nv / 2u));
             std::copy_n((int8_t *)&c, Nv / 2u, beta);
         }
     }
@@ -214,10 +246,7 @@ struct rate_1_container<int8_t, Nv> {
                 _mm_storeu_si128((__m128i *)&beta[i], _mm_blendv_epi8(_mm_set1_epi8(0), _mm_set1_epi8(1), alpha_vec));
             }
         } else if constexpr (Nv > 1u) {
-            int8_t alpha_vec[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin(), Nv, alpha_vec);
-
-            __m128i c = _mm_blendv_epi8(_mm_set1_epi8(0), _mm_set1_epi8(1), *(__m128i *)alpha_vec);
+            __m128i c = _mm_blendv_epi8(_mm_set1_epi8(0), _mm_set1_epi8(1), load_vec<Nv>(alpha.data()));
             std::copy_n((int8_t *)&c, Nv, beta);
         } else {
             beta[0] = std::signbit(alpha[0]);
@@ -247,10 +276,7 @@ struct rep_container<int8_t, Nv> {
                 _mm_storeu_si128((__m128i *)&beta[i], _mm_set1_epi8(std::signbit(sum)));
             }
         } else if constexpr (Nv > 2u) {
-            int8_t alpha_vec[16] __attribute__ ((aligned (16))) = {};
-            std::copy_n(alpha.begin(), Nv, alpha_vec);
-
-            __m128i acc = _mm_cvtepi8_epi16(*(__m128i *)alpha_vec);
+            __m128i acc = _mm_cvtepi8_epi16(load_vec<Nv>(alpha.data()));
             acc = _mm_hadd_epi16(acc, acc);
             acc = _mm_hadd_epi16(acc, acc);
             acc = _mm_hadd_epi16(acc, acc);
